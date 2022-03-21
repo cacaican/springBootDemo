b2b516fe2b53a5a8b718788d3cf0eb01f4aefb32
xiaocai <41604726+cacaican@users.noreply.github.com>
2022-03-21 17:51:38 +0800

    --尝试集成drools

---
+++ src/main/java/com/xiaocai/springboot/integration/rule/drools/entity/Person.java
@@ -0,0 +1,28 @@
+package com.xiaocai.springboot.integration.rule.drools.entity;
+
+/**
+ * @description:
+ * @author: xiaocai
+ * @time: 2022/3/21 17:29
+ */
+public class Person {
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
